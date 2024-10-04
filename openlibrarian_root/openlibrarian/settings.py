63130b69d1aa355bd10a6945411cf8c519287211
Rydal <62206239+RydalWater@users.noreply.github.com>
2024-10-04 10:14:57 +0100

    Logging config
    Added logging config to setting.py to support debugging post-deployment

--- openlibrarian_root/openlibrarian/settings.py
+++ openlibrarian_root/openlibrarian/settings.py
@@ -164,3 +164,23 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'error.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
