b5bb759089da91f078a75e0e4b27b9d0dace08d3
Mynima <62206239+RydalWater@users.noreply.github.com>
2024-11-09 21:36:45 +0000

    Test Cases (Index & Login)
    - Add functional and unit tests for index
    - Add functional and unit tests for login
    - Add functional and unit tests for login-npub
    - Add functional and unit tests for login-nsec
    - Add functional and unit tests for login-seed
    - Minor updates to base/index/login/login_npub/login_nsec/login_seed templates for id values and corrections based on test results.
    - circulation desk forms to add id values.

---
+++ openlibrarian_root/circulation_desk/tests/test_login_nsec.py
@@ -0,0 +1,79 @@
+from .test_index import BaseFunctionalTest, BaseUnitTests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class LoginNsecFunctionalTestCase(BaseFunctionalTest):
+    """
+    Functional Tests for the login page
+    """
+    def setUp(self):
+        """
+        Set Up and instantiate driver
+        """
+        self.url = "/login-nsec/"
+        self.driver = webdriver.Firefox()
+    
+    def test_redirect_npub(self):
+        """
+        Automatic redirect when logged in (NPUB)
+        """
+        session = self.client.session
+        session["npub"] = "npub1dpzan5jvyp0kl0sykx29397f7cnazgwa3mtkfyt8d9gga7htm9xsdsk85n"
+        session.save()
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.assertIn("/", self.driver.current_url)
+    
+    def test_redirect_nsec(self):
+        """
+        Automatic redirect when logged in (NSEC)
+        """
+        session = self.client.session
+        session["nsec"] = "nsec13m07g3kktrjjcfft27rekza8k8wkkunhp3rnv24lqe0n5yeg0k8s05xwhm"
+        session["npub"] = "npub1dpzan5jvyp0kl0sykx29397f7cnazgwa3mtkfyt8d9gga7htm9xsdsk85n"
+        session.save()
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.assertIn("/", self.driver.current_url)
+    
+    def test_invalid_nsec(self):
+        """
+        Test Invalid NSEC value
+        """
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.driver.find_element(by=By.ID, value="nsec").send_keys("nsecmadeup123456blahblah")
+        self.driver.find_element(by=By.ID, value="submit").click()
+        self.assertIn("/login-nsec/", self.driver.current_url)
+        self.assertIn("Invalid NSEC", self.driver.page_source)
+    
+    def test_valid_nsec(self):
+        """
+        Test Valid NPUB value
+        """
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.driver.find_element(by=By.ID, value="nsec").send_keys("nsec13m07g3kktrjjcfft27rekza8k8wkkunhp3rnv24lqe0n5yeg0k8s05xwhm")
+        self.driver.find_element(by=By.ID, value="submit").click()
+        self.assertIn("/", self.driver.current_url)
+    
+    def test_back(self):
+        """
+        Login with Back Button
+        """
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.driver.find_element(by=By.ID, value="back").click()
+        self.assertIn("/login/", self.driver.current_url)
+    
+    def tearDown(self):
+        """
+        Tear Down function to close driver
+        """
+        self.driver.close()
+
+class LoginNsecUnitTestCase(BaseUnitTests):
+    """
+    Unit Tests for the login page (NSEC)
+    """
+    def setUp(self):
+        self.url = "/login-nsec/"
+        self.template = "circulation_desk/login_nsec.html"
+        self.content = ["Log-in", "NSEC (read/write)", "Back"]
+        
---
+++ openlibrarian_root/circulation_desk/tests/test_login_seed.py
@@ -0,0 +1,101 @@
+from .test_index import BaseFunctionalTest, BaseUnitTests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class LoginSeedFunctionalTestCase(BaseFunctionalTest):
+    """
+    Functional Tests for the login page
+    """
+    def setUp(self):
+        """
+        Set Up and instantiate driver
+        """
+        self.url = "/login-seed/"
+        self.driver = webdriver.Firefox()
+    
+    def test_redirect_npub(self):
+        """
+        Automatic redirect when logged in (NPUB)
+        """
+        session = self.client.session
+        session["npub"] = "npub1dpzan5jvyp0kl0sykx29397f7cnazgwa3mtkfyt8d9gga7htm9xsdsk85n"
+        session.save()
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.assertIn("/", self.driver.current_url)
+    
+    def test_redirect_nsec(self):
+        """
+        Automatic redirect when logged in (NSEC)
+        """
+        session = self.client.session
+        session["nsec"] = "nsec13m07g3kktrjjcfft27rekza8k8wkkunhp3rnv24lqe0n5yeg0k8s05xwhm"
+        session["npub"] = "npub1dpzan5jvyp0kl0sykx29397f7cnazgwa3mtkfyt8d9gga7htm9xsdsk85n"
+        session.save()
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.assertIn("/", self.driver.current_url)
+    
+    def test_invalid_seed(self):
+        """
+        Test Invalid Seed Words
+        """
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.driver.find_element(by=By.ID, value="word1").send_keys("apple")
+        self.driver.find_element(by=By.ID, value="word2").send_keys("banana")
+        self.driver.find_element(by=By.ID, value="word3").send_keys("cherry")
+        self.driver.find_element(by=By.ID, value="word4").send_keys("date")
+        self.driver.find_element(by=By.ID, value="word5").send_keys("egg")
+        self.driver.find_element(by=By.ID, value="word6").send_keys("fig")
+        self.driver.find_element(by=By.ID, value="word7").send_keys("grape")
+        self.driver.find_element(by=By.ID, value="word8").send_keys("honey")
+        self.driver.find_element(by=By.ID, value="word9").send_keys("ice")
+        self.driver.find_element(by=By.ID, value="word10").send_keys("juice")
+        self.driver.find_element(by=By.ID, value="word11").send_keys("kiwi")
+        self.driver.find_element(by=By.ID, value="word12").send_keys("lemon")
+        self.driver.find_element(by=By.ID, value="submit").click()
+        self.assertIn("/login-seed/", self.driver.current_url)
+        self.assertIn("Invalid Seed", self.driver.page_source)
+    
+    def test_valid_npub(self):
+        """
+        Test Valid Seed Words
+        """
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.driver.find_element(by=By.ID, value="word1").send_keys("engine")
+        self.driver.find_element(by=By.ID, value="word2").send_keys("survey")
+        self.driver.find_element(by=By.ID, value="word3").send_keys("rich")
+        self.driver.find_element(by=By.ID, value="word4").send_keys("year")
+        self.driver.find_element(by=By.ID, value="word5").send_keys("woman")
+        self.driver.find_element(by=By.ID, value="word6").send_keys("keen")
+        self.driver.find_element(by=By.ID, value="word7").send_keys("thrive")
+        self.driver.find_element(by=By.ID, value="word8").send_keys("clip")
+        self.driver.find_element(by=By.ID, value="word9").send_keys("patrol")
+        self.driver.find_element(by=By.ID, value="word10").send_keys("patrol")
+        self.driver.find_element(by=By.ID, value="word11").send_keys("next")
+        self.driver.find_element(by=By.ID, value="word12").send_keys("quantum")
+        self.driver.find_element(by=By.ID, value="submit").click()
+        self.assertIn("/", self.driver.current_url)
+    
+    def test_back(self):
+        """
+        Login with Back Button
+        """
+        self.driver.get(f"http://127.0.0.1:8000{self.url}")
+        self.driver.find_element(by=By.ID, value="back").click()
+        self.assertIn("/login/", self.driver.current_url)
+    
+    def tearDown(self):
+        """
+        Tear Down function to close driver
+        """
+        self.driver.close()
+
+class LoginSeedUnitTestCase(BaseUnitTests):
+    """
+    Unit Tests for the login page (Seed)
+    """
+    def setUp(self):
+        self.url = "/login-seed/"
+        self.template = "circulation_desk/login_seed.html"
+        self.content = ["Log-in", "Seed Words (read/write)", "Back"]
+        
