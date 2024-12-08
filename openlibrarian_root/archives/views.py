b96dffd5637b0fead091ea9c59a9fdbc86fc74de
Mynima <62206239+RydalWater@users.noreply.github.com>
2024-12-08 09:19:47 +0000

    Archives Session
    - Updated archives pages to pull session data
    - Back button only available when not logged in

--- openlibrarian_root/archives/views.py
+++ openlibrarian_root/archives/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
 from utils.Releases import get_release_info
+from utils.Session import async_get_session_info
 
 # About Page
 async def about(request):
     """View for the About page of the website."""
+    session = await async_get_session_info(request)
+    context = {
+        'session': session
+    }
     # TODO: Update about page content to include more about the project
-    return render(request, 'archives/about.html')
+    return render(request, 'archives/about.html', context)
 
 async def updates(request):
     """View for the Updates page of the website."""
     release_info = await get_release_info()
+    session = await async_get_session_info(request)
     context = {
-        'release_info': release_info
+        'release_info': release_info,
+        'session': session
     }
     return render(request, 'archives/updates.html', context)
 
-def privacy(request):
+async def privacy(request):
     """View for the Privacy page of the website."""
-    return render(request, 'archives/privacy.html')
+    session = await async_get_session_info(request)
+    context = {
+        'session': session
+    }
+    return render(request, 'archives/privacy.html', context)
 
 # 404 Page
 def page_not_found_view(request, exception):
