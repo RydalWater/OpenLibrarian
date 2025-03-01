0a5b85307c14a9d0da3752a573cca1e42b23a464
Mynima <62206239+RydalWater@users.noreply.github.com>
2025-03-01 16:22:02 +0000

    Card Placeholder
    - Added Placeholder element on Library Card to improve load experience
    - Moved button function to front end on library card and replaced with card_data and explore_profile JsonResponse views
    - Included urls for new response views

--- openlibrarian_root/library_card/urls.py
+++ openlibrarian_root/library_card/urls.py
@@ -5,4 +5,10 @@ app_name = 'library_card'
 urlpatterns = [
     # Library Card Index page
     path('<str:npub>/', views.library_card, name='library_card'),
+
+    # Library Card Data Fetch
+    path('card-data/<str:npub>', views.card_data, name='card_data'),
+
+    # Libary Card Explore Profile
+    path('explore-profile/<str:npub>', views.explore_profile, name='explore_profile'), 
 ]
--- openlibrarian_root/library_card/views.py
+++ openlibrarian_root/library_card/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.core.cache import cache
+from django.shortcuts import render
+from django.http import JsonResponse
 from utils.Session import async_logged_in, async_get_session_info, async_set_session_info
 from utils.Profile import fetch_profile_info
 from utils.Library import fetch_libraries, prepare_libraries
@@ -8,8 +8,6 @@ from utils.Login import check_npub
 from utils.Progress import fetch_progress
 from utils.Constants import INTERESTS_HASHMAP
 import asyncio
-
-
 
 async def fetch_user_data(npub: str, read_only: bool = False):
     """Fetch user data from relays"""
@@ -27,115 +25,98 @@ async def fetch_user_data(npub: str, read_only: bool = False):
                     isbns.append(book["i"])
     progress = await fetch_progress(npub=npub, isbns=isbns, relays=relays)
 
-    return profile, relays, added_relays, libraries, interest_list, progress
+    return profile, relays, added_relays, libraries, interest_list, progress    
 
-async def library_card(request, npub: str=None):
-    """Returns Simple view for Library."""
-    # Return to index if the user profile is not logged in]
-    if npub in (None, ""):
-        return redirect('circulation_desk:index')
+def library_card(request, npub: str=None):
+    """Returns view for Library."""
+    return render(request, 'library_card/card.html')
+
+async def card_data(request, npub: str=None):
+    """Returns JSONResponse for Library card data."""
+    if await async_logged_in(request):
+        session = await async_get_session_info(request)
     else:
-        if request.method == 'GET':
-            if await async_logged_in(request):
-                session = await async_get_session_info(request)
-            else:
-                session = None
-            
-            if session and npub == session['npub']:
-                nym = session['nym']
-                profile = session['profile']
-                interest_list = session['interests']
-                libraries = session['libraries']
-                progress = session['progress']
-                owner = True
-            else:
-                profile, relays, added_relays, libraries, interest_list, progress = await fetch_user_data(npub=npub, read_only=True)
-                nym = profile['nym']
-                owner = False
+        session = None
+    
+    if session and npub == session['npub']:
+        nym = session['nym']
+        profile = session['profile']
+        interest_list = session['interests']
+        libraries = session['libraries']
+        progress = session['progress']
+        owner = True
+    else:
+        profile, relays, added_relays, libraries, interest_list, progress = await fetch_user_data(npub=npub, read_only=True)
+        nym = profile['nym']
+        owner = False
 
-            displayname = profile['displayname']
-            about = profile['about']
-            picutre = profile['picture']
-            interests = []
-            for key, value in INTERESTS_HASHMAP.items():
-                for interest in interest_list:
-                    if value == interest:
-                        interests.append(key)
-                        break
-            if len(interests) == 0:
-                interests.append("No interests set, they are an enigma!")
+    displayname = profile['displayname']
+    about = profile['about']
+    picutre = profile['picture']
+    interests = []
+    for key, value in INTERESTS_HASHMAP.items():
+        for interest in interest_list:
+            if value == interest:
+                interests.append(key)
+                break
+    if len(interests) == 0:
+        interests.append("No interests set, they are an enigma!")
 
-            current_books = {}
-            to_read = 0
-            read = 0
-            current = 0
-            for library in libraries:
-                if library["s"] == "CR":
-                    for book in library["b"]:
-                        current += 1
-                        if book["h"] == "N":
-                            current_books[f"{current}"] = {"t": book["t"], "a": book["a"], "i" : book["i"]}
-                        else:
-                            current_books[f"{current}"] = {"t": "A Mysterious Book", "a": "Unknown Author", "i" : "hidden"}
-                elif library["s"] in("TRS", "TRW"):
-                    for book in library["b"]:
-                        to_read += 1
-                elif library["s"] == "HR":
-                    for book in library["b"]:
-                        read += 1
-            
-            if current_books == {}:
-                current_books[f"{current}"] = {"t": "This user is currently between books."}
+    current_books = {}
+    to_read = 0
+    read = 0
+    current = 0
+    for library in libraries:
+        if library["s"] == "CR":
+            for book in library["b"]:
+                current += 1
+                if book["h"] == "N":
+                    current_books[f"{current}"] = {"t": book["t"], "a": book["a"], "i" : book["i"]}
+                else:
+                    current_books[f"{current}"] = {"t": "A Mysterious Book", "a": "Unknown Author", "i" : "hidden"}
+        elif library["s"] in("TRS", "TRW"):
+            for book in library["b"]:
+                to_read += 1
+        elif library["s"] == "HR":
+            for book in library["b"]:
+                read += 1
+    
+    if current_books == {}:
+        current_books[f"{current}"] = {"t": "This user is currently between books."}
 
-            context = {
-                'nym': nym,
-                'displayname': displayname,
-                'about': about,
-                'picture': picutre,
-                'interests': interests,
-                'current_books': current_books,
-                'to_read': to_read,
-                'read': read,
-                'owner': owner,
-                'progress': progress
-            }
-            
-            return render(request, 'library_card/card.html', context)
-        elif request.method == 'POST':
-            # Head back to home if already logged in
-            if "home" in request.POST:
-                return redirect('circulation_desk:index')
-            
-            # Logout and then in as read-only for that npub
-            if "explore" in request.POST:
+    context = {
+        'nym': nym,
+        'displayname': displayname,
+        'about': about,
+        'picture': picutre,
+        'interests': interests,
+        'current_books': current_books,
+        'to_read': to_read,
+        'read': read,
+        'owner': owner,
+        'progress': progress
+    }
+    return JsonResponse(context)
 
-                # First log out of session and clear cache (including nsec)
-                request.session.flush()
-                cache.clear()
+async def explore_profile(request, npub: str=None):
+    """Log-in via npub and return JSONResponse."""
+    
+    # Then log-in via npub
+    valid_npub = check_npub(npub)
+    if valid_npub:
+        # Fetch Profile Info and set Session Data
+        profile, relays, added_relays, libraries, interests, progress = await fetch_user_data(npub=npub, read_only=True)
+        nym = profile.get('nym')
 
-                # Then log-in via npub
-                valid_npub = check_npub(npub)
-                if valid_npub:
-                    # Fetch Profile Info and set Session Data
-                    profile, relays, added_relays, libraries, interests, progress = await fetch_user_data(npub=npub, read_only=True)
-                    nym = profile.get('nym')
+        # Get list of ISBNs and then create progress object
+        isbns = []
+        for library in libraries:
+            if library["s"] == "CR":
+                for book in library["b"]:
+                    if "Hidden" not in book["i"]:
+                        isbns.append(book["i"])
+        progress = await fetch_progress(npub=npub, isbns=isbns, relays=relays)
 
-                    # Get list of ISBNs and then create progress object
-                    isbns = []
-                    for library in libraries:
-                        if library["s"] == "CR":
-                            for book in library["b"]:
-                                if "Hidden" not in book["i"]:
-                                    isbns.append(book["i"])
-                    progress = await fetch_progress(npub=npub, isbns=isbns, relays=relays)
+        await async_set_session_info(request, npub=npub, nym=nym, relays=relays, def_relays=added_relays, profile=profile, interests=interests, libraries=libraries, progress=progress)
 
-                    await async_set_session_info(request, npub=npub, nym=nym, relays=relays, def_relays=added_relays, profile=profile, interests=interests, libraries=libraries, progress=progress)
-
-                return redirect('circulation_desk:index')
-            
-            # Clear session and head back to create and account
-            if "new" in request.POST:
-                # First make sure logged out of session and clear cache (including nsec)
-                request.session.flush()
-                cache.clear()
-                return redirect('circulation_desk:create-account')
+    return JsonResponse({'success': True})
